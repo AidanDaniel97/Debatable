8b9b03e96cc340af9319deda6afa25af25782d9e
Aidan Daniel <aidan.d@wearedrum.com>
2017-02-13 20:01:47 +0000

    Changes to homepage, now using EJS for templating, loops through database and collections all the topics, lays them out on the page

--- server/server.js
+++ server/server.js
@@ -1,5 +1,3 @@
-
-
 var express = require('express'); 
 var app = express();   
 var path = require('path');
@@ -7,16 +5,19 @@ var server = app.listen(process.env.PORT || 5000,function(){
 
   console.log("Express server listening in %s mode", process.env.PORT, app.settings.env);
 });
-
 var io = require('socket.io').listen(server);
- 
+
+
 //Database - mongodb
 var mongodb = require("mongodb"); 
 app.use(express.static(path.join(__dirname, '../client')));
 
 
-queues_holder = [];
+
 socket_list = {};
+queues_holder = [];
+
+var queue_manager = require("../server/modules/queue_manager");
 
 //Routing
 app.set("view engine","ejs");
@@ -24,7 +25,21 @@ app.set("view engine","ejs");
 //Respond to get req
 var path = require('path');
  
- 
+function get_date(){
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+
+	var yyyy = today.getFullYear();
+	if(dd<10){
+	    dd='0'+dd;
+	} 
+	if(mm<10){
+	    mm='0'+mm;
+	} 
+	var today = dd+'/'+mm+'/'+yyyy;
+	return today;
+}
 
 
 
@@ -32,6 +47,8 @@ app.get("/", function(req,res){
 	console.log("Requested Homepage")
 	var MongoClient = mongodb.MongoClient;
 	var mongoUrl = "mongodb://127.0.0.1:27017/debatable";
+	var server_date = get_date();
+
 
 MongoClient.connect(mongoUrl,function(err,db){
 	if(err){
@@ -44,11 +61,11 @@ MongoClient.connect(mongoUrl,function(err,db){
 		topics.find().toArray(function(err,result){ 
 			if (err){
 				console.log("Error retrieving database collection");
-			}else if (result){ 
-			  res.render("index",{'topic_list':result});
+			}else if (result){  
+				res.render("index",{'topic_list':result,'current_date':server_date});
 			}else{
 				console.log("No data found in collection");
-				 res.render("index",{'topic_list':result});
+				res.render("index",{'topic_list':result,'current_date':server_date});
 			}
 
 			db.close();
@@ -72,92 +89,14 @@ app.get("/chat/:debate_name/:debate_mode", function(req,res){
 
 
 
-function randomString(length) {
-    return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
-}
-
-
-
-function join_debate(socket,debate_name){    
-    console.log("Joined the debate " + debate_name);
-    if (queues_holder[debate_name] == undefined){
-    	queues_holder[debate_name] = [];
-    } 
-    queues_holder[debate_name][socket.id] = socket.id;
-    console.log("===== " , queues_holder)
-}
-
-
 function spectate_debate(socket,debate_name){    
     console.log("Spectating the debate " + debate_name);
     
 }
 
 
-function join_debate_queue(socket,debate_name,debate_side){
-	//queues_holder["trump"] = ["123","666","987"]; 
-	if (!queues_holder[debate_name]){
-		queues_holder[debate_name] = [];
-	}
-
-	if (!queues_holder[debate_name][debate_side]){
-		queues_holder[debate_name][debate_side] = [];
-	}
-	//queues_holder.trump.push("hello")
-	queues_holder[debate_name][debate_side].push(socket.id) //add socket id to list
-	console.log("User has joined debate queue ====== " + queues_holder[debate_name][debate_side].length);
-
-	check_debate_queue(debate_name,debate_side);
-};
 
 
-function check_debate_queue(debate_name, debate_side){
-
-	if (debate_side == "agree"){
-		if(queues_holder[debate_name]["disagree"] && queues_holder[debate_name]["disagree"].length  >= 1){
-			create_debate_room(debate_name);
-		}else{
-			console.log("Not enough people in the " + debate_name + " disagree queue to start a debate.")
-		}
-	}else{
-		if(queues_holder[debate_name]["agree"] && queues_holder[debate_name]["agree"].length >= 1){
-			create_debate_room(debate_name);
-		}else{
-			console.log("Not enough people in the " + debate_name + " agree queue to start a debate.")
-		}
-	}
-
-	console.log(queues_holder[debate_name][debate_side].length , queues_holder[debate_name]["agree"].length)
-
-
-};
-
-
- 
-function create_debate_room(debate_name){
-	room_id = randomString(16);
-	console.log("New room created ", room_id);
-	//debate_queue.trump.agree[0]
-	for (i = 0; i <= 1; i++) {//get sockets of first 2 people in queue  
-    	if (i == 0){
-    		current_socket_id = queues_holder[debate_name]["disagree"][0];
-    	}else{
-    		current_socket_id = queues_holder[debate_name]["agree"][0];
-    	}
-    	current_socket = socket_list[current_socket_id]; 
-    	current_socket.join(room_id);
-    	console.log("User joined a new room")
-    	room_data = {'debate_name':debate_name,'room_id':room_id};
-    	current_socket.emit("joined_room",room_data);
-   	}
-	//Remove these users from the queue
- 	console.log("before: " + queues_holder[debate_name]["agree"])
- 	queues_holder[debate_name]["agree"] = queues_holder[debate_name]["agree"].splice(1);//remove the two 
- 	queues_holder[debate_name]["disagree"] = queues_holder[debate_name]["disagree"].splice(1);//remove the two  
- 	console.log("After: " + queues_holder[debate_name]["agree"])
-
-
-}
 
 
 
@@ -177,7 +116,7 @@ io.on('connection', function(socket){
   	//join_debate(socket,debate_name);
   	//Join debate queue
 
-  	join_debate_queue(socket, debate_name, debate_side);
+  	queue_manager.join_debate_queue(socket, debate_name, debate_side);
 
   }); 
 
